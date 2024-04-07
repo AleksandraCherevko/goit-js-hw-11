2e99a124e165fc247d1c3120fe759105af5779e8
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2024-04-07 19:10:51 +0000

    Deploying to gh-pages from @ AleksandraCherevko/goit-js-hw-11@f39308b2ab882d8cdfa7f3ff8d3046a93333226a 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import{i as f,S as h}from"./assets/vendor-0fc460d7.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function d(s){return s.map(({webformatURL:o,largeImageURL:r,tags:i,likes:e,views:t,comments:a,downloads:u})=>` <li class="gallery-item">
+import{i as u,S as d}from"./assets/vendor-0fc460d7.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&l(o)}).observe(document,{childList:!0,subtree:!0});function r(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function l(e){if(e.ep)return;e.ep=!0;const t=r(e);fetch(e.href,t)}})();function m(i){return i.map(({webformatURL:s,largeImageURL:r,tags:l,likes:e,views:t,comments:o,downloads:n})=>` <li class="gallery-item">
         <a class="gallery-link" href="${r}">
           <img
             class="gallery-image"
-            src="${o}"
-            alt="${i}"
+            src="${s}"
+            alt="${l}"
             width="360"
           />
         </a>
@@ -18,12 +18,12 @@ import{i as f,S as h}from"./assets/vendor-0fc460d7.js";(function(){const o=docum
           </div>
           <div class="block">
             <h2 class="tittle">Comments</h2>
-            <p class="amount">${a}</p>
+            <p class="amount">${o}</p>
           </div>
           <div class="block">
             <h2 class="tittle">Downloads</h2>
-            <p class="amount">${u}</p>
+            <p class="amount">${n}</p>
           </div>
         </div>
-      </li>`).join("")}function c(s){const o="https://pixabay.com/api/",r="43242855-c6b7005837cbd7f2bbf3eb2ae";s.includes(" ")&&s.replace(/\s+/g,"+");const i=new URLSearchParams({key:r,q:s,image_type:"photo",orientation:"horizontal",safesearch:!0});return fetch(`${o}?${i}`).then(e=>{if(!e.ok)throw new Error("Failed to fetch images");return e.json()})}const n=document.querySelector(".js-form"),l=document.querySelector(".gallery");n.addEventListener("submit",m);function m(s){s.preventDefaul(),l.innerHTML="";const o=s.target.elements.search.value.trim();c(o).then(r=>{r.hits.length||f.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),l.innerHTML=d(r.hits),new h(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}),c.refresh(),n.reset()}).catch(r=>{console.log(r)})}
+      </li>`).join("")}const c=document.querySelector(".js-form"),a=document.querySelector(".gallery");c.addEventListener("submit",f);function f(i){i.preventDefaul(),a.innerHTML="";const s=i.target.elements.search.value.trim();getPicture(s).then(r=>{r.hits.length||u.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"}),a.innerHTML=m(r.hits),new d(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250}),refreshPage.refresh(),c.reset()}).catch(r=>{console.log(r)})}
 //# sourceMappingURL=commonHelpers.js.map
